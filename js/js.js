4a4a5e3ff5ed7cfc9cd92f6f0f7776842a76a61e
Roman Macak <r.macak@seznam.cz>
2016-01-26 17:30:35 +0100

    Calculator working
    Missing some features

--- js/js.js
+++ js/js.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 
-});
-
-function calc () {
+function calculator () {
   var memory = 0;
   var operator = "";
-  var display = 0;
+  var display = "0";
 
-  function putNumber (num) {
-    if (display === 0) {
+  this.init = function () {
+    memory = 0;
+    operator = "";
+    display = "0";
+    this.refresh();
+  }
+
+  this.putNumber = function (num) {
+    this.logit();
+    if (display === "0") {
       display = num;
       this.refresh();
     }
@@ -18,52 +24,78 @@ function calc () {
     }
   }
 
-  function putOperator (ope) {
-    if (operator === "=") {
+  this.putOperator = function (ope) {
+    this.logit();
+    if (ope === "=" || operator !== "") {
       this.compute()
     }
     else {
+      memory = parseFloat(display);
+      display = "0";
       operator = ope;
+      this.refresh();
     }
   }
 
-  function compute () {
-    switch (operator) {
-      case "-":
-        display = memory - display;
-        this.refresh();
-        break;
-      case "+":
-        display = memory + display;
-        this.refresh();
-        break;
-      case "*":
-        display *= memory;
-        this.refresh();
-        break;
-      case "/":
-        display = memory / display;
-        this.refresh();
-        break;
-      default:
+  this.putSpecial = function (spec) {
+    this.logit();
+    if (spec === "AC") {
+      this.init()
+    } else if (spec === "CE") {
+      display = "0";
+      this.refresh();
     }
-}
-  function refresh() {
-    $("#display").html(display.substr(0,10));
+  }
+
+  this.compute = function () {
+    displayInt = parseFloat(display);
+    this.logit();
+    if (operator === "-"){
+        display = memory - displayInt;
+      } else if (operator === "+") {
+        display = memory + displayInt;
+      } else if (operator === "*") {
+        display = memory * displayInt;
+      } else if (operator === "/") {
+        if (displayInt !== 0) {
+          display = memory / displayInt;
+        }
+      } else if (operator === "%") {
+        if (displayInt !== 0) {
+          display = (memory / displayInt)*100;
+        }
+      }
+    memory = parseFloat(display);
+    operator = "";
+    this.refresh();
+  }
+  this.refresh = function () {
+    $("#display").html(display);
+  }
+  this.logit = function () {
+    console.log("display " + display);
+    console.log("memory " + memory);
+    console.log("operator " + operator);
   }
 };
-
-
-
+var calc = new calculator();
 $(".numbers").click(function(event) {
    // get button id to get number
-  calc.putNumber($(this).id());
+  calc.putNumber($(this).attr("id"));
 });
 
 $(".operators").click(function(event) {
-  calc.putOperator($(this).id());
+  calc.putOperator($(this).attr("id"));
 });
 
 $("#compute").click(function() {
   calc.compute();
+});
+
+$(".special").click(function() {
+  calc.putSpecial($(this).attr("id"));
+});
+
+
+
 });
